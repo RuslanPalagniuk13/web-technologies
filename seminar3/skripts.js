3d3859654fc9b081077b826c6a45d576b2603296
Руслан Палагнюк <rlpalagniuk@mail.ru>
2023-02-02 17:16:29 +0300

    домашнее задание семинар 3 веб-технологии

---
+++ seminar3/skripts.js
@@ -0,0 +1,8 @@
+let userName = prompt('Привет! Как Вас зовут?', 'имя');
+
+    function showMessage() {
+        let message = (`Привет, ${userName}!`);
+        alert(message);
+    }
+        showMessage();
+
